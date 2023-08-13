deef670c8af85e337a00b6d656e01ab4aab28d4a
Erik Z <molagg92@gmail.com>
2023-08-13 14:09:26 -0700

    add skeleton

---
+++ Bakery/Controllers/SampleController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Week5CodeReview.Models;
+
+namespace Week5CodeReview.Controllers
+{
+    public class Week5CodeReview : Controller
+    {
+    }
+}
