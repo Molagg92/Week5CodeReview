b56086a0cce93f712923609a67c49bbd6afce16a
Erik Z <molagg92@gmail.com>
2023-08-13 19:08:04 -0700

    add model validation on Create page

--- Bakery/Models/Flavor.cs
+++ Bakery/Models/Flavor.cs
@@ -6,7 +6,7 @@ namespace Bakery.Models
   public class Flavor
   {
     public int FlavorId { get; set; }
-    // [Required(ErrorMessage = "The Flavor's name can't be empty!")]
+    [Required(ErrorMessage = "The Flavor's name can't be empty!")]
     public string Frosting { get; set; }
   
     public List<FlavorTreatEntity> FlavorTreatEntities { get; set; }
--- Bakery/Models/Treat.cs
+++ Bakery/Models/Treat.cs
@@ -6,7 +6,7 @@ namespace Bakery.Models
   public class Treat
   {
     public int TreatId { get; set; }
-    // [Required(ErrorMessage = "The Treat's type can't be empty!")]
+    [Required(ErrorMessage = "The Treat's type can't be empty!")]
     public string Type { get; set; }
     
     public List<FlavorTreatEntity> FlavorTreatEntities{ get; set; }
